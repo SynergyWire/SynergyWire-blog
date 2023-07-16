718a5aeb58722744345119fdcd336699a0fe657a
Sierra <m.sierra1037@hotmail.com>
2023-07-16 17:07:43 -0500

    feat: jsless navbar functionality

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,46 +1,62 @@
-import React from "react"
-import Link from 'next/link'
-import Icon from '../icons/Icon'
-import { ICON_KEYS } from '@/utils/contants'
-import "./header.css"
+import React from "react";
+import Link from "next/link";
+import Icon from "../icons/Icon";
+import { ICON_KEYS } from "@/utils/contants";
+import "./header.css";
 
 export default function Header() {
   return (
     <header className="max-w-screen border-b border-solid border-black p-8 flex justify-between content-center">
-      <nav>
-        <Link className='font-bold p1' href="./">About</Link>
+      <nav className="header-links">
+        <Link className="font-bold p1" href="./">
+          About
+        </Link>
       </nav>
 
-
-      <Link href="/" >
-        <h1 className="text-2xl font-bold hover:text-yellow-600">
-          Synergy Wire Blog</h1>
-      </Link>
-      <div>      <Link
-        className="text-slate-400"
-        href="https://github.com/SynergyWire"
-        target="_blank"
-      >
-        <Icon name={ICON_KEYS.GITHUB} />
-      </Link></div>
+      <h1 className="text-2xl font-bold hover:text-yellow-600">
+        <Link href="/">Synergy Wire Blog </Link>
+      </h1>
 
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" /></svg><input type="checkbox" id="open"/>
-        <ul>
-      <li>
-        <a href="#">
-          <i className="fa fa-home"></i>
-          <span>Home</span>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i className="fa fa-paint-brush"></i>
-          <span>Portfolio</span>
-        </a>
-      </li>
-    </ul>
+        <Link
+          className="text-slate-400 organization-link"
+          href="https://github.com/SynergyWire"
+          target="_blank"
+        >
+          <Icon name={ICON_KEYS.GITHUB} />
+        </Link>
       </div>
+      <label className="responsive-menu" htmlFor="toggle-button">
+        <svg
+          className="menu-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="48"
+          viewBox="0 -960 960 960"
+          width="48"
+        >
+          <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+        </svg>
+        <input type="checkbox" id="toggle-button" />
+
+        <ul className="responsive-menu__content">
+          <li>
+            <Link href={"#"}>About</Link>
+          </li>
+          <li>
+            <Link href={"#"}>Authors</Link>
+          </li>
+
+          <div>
+            <Link
+              className="text-slate-400"
+              href="https://github.com/SynergyWire"
+              target="_blank"
+            >
+              <Icon name={ICON_KEYS.GITHUB} />
+            </Link>
+          </div>
+        </ul>
+      </label>
     </header>
-  )
+  );
 }
