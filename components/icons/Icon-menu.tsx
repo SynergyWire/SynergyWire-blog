60ae7a662c690ec9a0628e4338f389573ebf63b7
Mateo SIerra <m.sierra1037@hotmail.com>
2023-07-19 17:32:37 -0600

    fix: needed classname on Icon-menu.tsx

--- components/icons/Icon-menu.tsx
+++ components/icons/Icon-menu.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function IconMenu () {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg"height="48" width="48" viewBox="0 -960 960 960">
+    <svg className="menu-icon-burger" xmlns="http://www.w3.org/2000/svg"height="48" width="48" viewBox="0 -960 960 960">
     <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
   </svg>
   )
