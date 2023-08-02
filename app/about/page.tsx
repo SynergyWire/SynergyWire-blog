a05c4259b7cc43f69d8035b528ba865368f5aa7f
Sierra <m.sierra1037@hotmail.com>
2023-08-01 22:50:39 -0500

    feat: finished about component, checked content on grammarly

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,24 +1,14 @@
-import React from "react";
-import Icon from "@/components/icons/Icon";
-import "./styles.css";
+import React from 'react';
+import { Author, AuthorCard } from '../../components/AuthorCard';
+import './styles.css';
 
 const page = () => {
-  interface Author {
-    name: string;
-    pictureUrl: string;
-    introduction: string;
-    socials: {
-      linkedIn?: string;
-      github?: string;
-      personalWebsite?: string;
-    };
-  }
-
   const authorList: Author[] = [
     {
       name: "Mateo Sierra",
       pictureUrl: "https://avatars.githubusercontent.com/u/71465895?v=4",
-      introduction: "Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple ",
+      introduction:
+        "Experienced frontend dev & aspiring digital illustrator. Crafting captivating and user-centric web experiences with clean code & artistic flair.",
       socials: {
         linkedIn: "https://www.linkedin.com/in/mateo-sierra-4b70951b7/",
         github: "https://github.com/Sierra9999",
@@ -26,7 +16,8 @@ const page = () => {
     },
     {
       name: "Luis Alexander",
-      introduction: "Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple Apple ",
+      introduction:
+        "Experienced system engineer specialized in development, adept at leading projects, analyzing challenges, and delivering exceptional results. Passionate problem-solver.",
       pictureUrl: "https://avatars.githubusercontent.com/u/26258524?v=4",
       socials: {
         linkedIn: "https://www.linkedin.com/in/luis-alexander-b83478184/",
@@ -35,56 +26,22 @@ const page = () => {
     },
   ];
 
-  const AuthorCard = (author: Author) => {
-    const { github, linkedIn } = author.socials;
-
-    return (
-      <section className="author-card">
-        <h2 className="author-card__name">{author.name}</h2>
-        <img
-          src={author.pictureUrl}
-          alt={`${author.name}'s profile picture on github`}
-          className="author-card__picture"
-        />
-        <p className="author-card__introduction">{author.introduction}</p>
-        <section className="author-card__socials">
-          {github ? (
-            <a
-              className="author-card__socials__anchor"
-              target="_blank"
-              href={github}
-            >
-              <Icon name={"GITHUB"}></Icon>
-            </a>
-          ) : null}
-          {linkedIn ? (
-            <a
-              className="author-card__socials__anchor"
-              target="_blank"
-              href={linkedIn}
-            >
-              <Icon name={"LINKEDIN"}></Icon>
-            </a>
-          ) : null}
-        </section>
-      </section>
-    );
-  };
-
   return (
-    <main  >
+    <main>
       <p className="about-paragraph">
         At SynergyWire, we're on an enchanting journey through the world of
-        software development, we enjoy growing in knowledge, sharing and helping
-        other developers in their daunting chase for guidance in this colosal,
-        fluctuating and unpredictible industry.
+        software development, we enjoy growing in knowledge in order to share
+        and help other developers in their daunting chase for guidance in this
+        colossal, fluctuating and unpredictable industry.
       </p>
       <p className="about-paragraph">
-        Wether you are a beginner or you had several experiences in the art of
-        software development, we believe constant growth in knowledge and skills
-        is a must to stay relevant in this realm, a software developer who is not exploring new tools and honing the ones already at disposition will eventually start experiencing undesired consequences 
+        Whether you are a beginner or you had several experiences in the art of
+        software development, we believe in constant growth in knowledge and
+        skills is a must to stay relevant in this realm, software developers who
+        are not exploring new tools and honing the ones already at their
+        disposition will eventually start experiencing undesired consequences
       </p>
-      <h2 className="about-authors" >About the authors</h2>
+      <h2 className="about-authors">About the authors</h2>
       <section className="writers">
         <AuthorCard {...authorList[1]} />
         <AuthorCard {...authorList[0]} />
