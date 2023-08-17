7a2b0cf465ec49f6eaf57350aae9f7e993dc7cde
Mateo Sierra <m.sierra1037@hotmail.com>
2023-08-17 14:37:01 -0600

    fix: fixed article responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,8 @@ export default function RootLayout({
       <body>
       <Header />
         <main className="mx-auto max-w-screen-xl min-h-screen px-6 pt-6 flex flex-col">
-          <div className="py-6 flex justify-center">
-            <main>
+          <div className="py-6">
              {children}
-            </main>
           </div>
           <Footer />
         </main>
